e543ee144460ec5c57c6a9eae3f32c245e02e15b
Mads Harder <mads987a@gmail.com>
2020-04-01 13:40:14 +0200

    Added player movement and animation

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movment;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+        movment.x = Input.GetAxisRaw("Horizontal");
+        movment.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movment.x);
+        animator.SetFloat("Vertical", movment.y);
+        animator.SetFloat("Speed", movment.sqrMagnitude);
+    }
+
+    void FixedUpdate() 
+    {
+        // Movment
+        rb.MovePosition(rb.position + movment * moveSpeed * Time.fixedDeltaTime);
+
+    }
+}
