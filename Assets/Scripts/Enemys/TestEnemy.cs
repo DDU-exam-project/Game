05b342fb92ef80ba298ebbdc47aeef558d96a566
Mads Harder <mads987a@gmail.com>
2020-04-02 13:18:56 +0200

    Added a test enemy, that follows you

---
+++ Assets/Scripts/Enemys/TestEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnemy : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public float minDist = 2f;
+
+    public Rigidbody2D rb;
+
+    GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if( Vector2.Distance(rb.transform.position, player.transform.position) >= minDist) {
+            rb.transform.position = Vector2.MoveTowards(
+                rb.transform.position,
+                player.transform.position,
+                moveSpeed * Time.deltaTime
+            );
+        }
+    }
+
+    void FixedUpdate() 
+    {
+        // Movment
+        //rb.MovePosition(rb.position + player.position * moveSpeed * Time.fixedDeltaTime);
+    }
+}
