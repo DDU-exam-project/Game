ca5727272c218ae8aef697fc951ca4e5577069da
Mads Harder <mads987a@gmail.com>
2020-04-15 14:08:42 +0200

    Added spotted play functionality

--- Assets/Scripts/Enemys/TestEnemy.cs
+++ Assets/Scripts/Enemys/TestEnemy.cs
@@ -2,21 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestEnemy : MonoBehaviour
-{
+public class TestEnemy : MonoBehaviour {
 
     public float moveSpeed = 5f;
     public float minDist = 2f;
+    public float maxDist = 2f;
 
     public Rigidbody2D rb;
 
     GameObject player;
 
+    private bool spottedPlayer = false;
+
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+
+        // Find player object
         player = GameObject.FindGameObjectWithTag("Player");
-        if( Vector2.Distance(rb.transform.position, player.transform.position) >= minDist) {
+
+        // If player is whitind maxDist, then player is spotted
+        if (!spottedPlayer & Vector2.Distance(rb.transform.position, player.transform.position) <= maxDist) spottedPlayer = true;
+
+        // If player is spotted and is not whitind minDist, then move towards player
+        if (spottedPlayer & Vector2.Distance(rb.transform.position, player.transform.position) >= minDist) {
+
+            Vector2 direction = player.transform.position - rb.transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+
             rb.transform.position = Vector2.MoveTowards(
                 rb.transform.position,
                 player.transform.position,
@@ -25,8 +38,7 @@ public class TestEnemy : MonoBehaviour
         }
     }
 
-    void FixedUpdate() 
-    {
+    void FixedUpdate() {
         // Movment
         //rb.MovePosition(rb.position + player.position * moveSpeed * Time.fixedDeltaTime);
     }
