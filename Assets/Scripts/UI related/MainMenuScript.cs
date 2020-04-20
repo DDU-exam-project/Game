29a67e9a9fd7604b92ca42e4953d326e56f64808
Omonom <maxih@hotmail.dk>
2020-04-20 13:30:13 +0200

    main menu works

--- Assets/Scripts/UI related/MainMenuScript.cs
+++ Assets/Scripts/UI related/MainMenuScript.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine.SceneManagement;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenuScript : MonoBehaviour
 {
-    [SerializeField] GameObject optionsFirstSelected, bacFromOptionsSelected;
+    [SerializeField] GameObject optionsFirstSelected, backFromOptionsSelected;
     [SerializeField] GameObject optionsPanel;
 
     public void PlayGame()
@@ -14,6 +15,18 @@ public class MainMenuScript : MonoBehaviour
     public void OnOptionsOpenClose()
     {
         optionsPanel.SetActive(!optionsPanel.activeInHierarchy);
+
+        //clear selection
+        EventSystem.current.SetSelectedGameObject(null);
+        //set new selction
+        if (optionsPanel.activeSelf)
+        {
+            EventSystem.current.SetSelectedGameObject(optionsFirstSelected);
+        }
+        else
+        {
+            EventSystem.current.SetSelectedGameObject(backFromOptionsSelected);
+        }
     }
 
     public void CloseGame()
