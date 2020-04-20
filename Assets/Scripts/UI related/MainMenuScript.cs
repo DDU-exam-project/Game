9ea92fade0d22131015c11d93b8d86c0e4615d73
Omonom47 <56996234+Omonom47@users.noreply.github.com>
2020-04-20 08:54:33 +0200

    Main menue now with funtionality + custom ui element

---
+++ Assets/Scripts/UI related/MainMenuScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    [SerializeField] GameObject optionsFirstSelected, bacFromOptionsSelected;
+    [SerializeField] GameObject optionsPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnOptionsOpenClose()
+    {
+        optionsPanel.SetActive(!optionsPanel.activeInHierarchy);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/UI related/OptionsMenuScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+public class OptionsMenuScript : MonoBehaviour
+{
+    [SerializeField] AudioMixer master;
+    [SerializeField] TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            options.Add(resolutions[i].width + " x " + resolutions[i].height);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+    }
+
+    public void SetVolume(float volume)
+    {
+        master.SetFloat("masterVolume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void ToggleFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
