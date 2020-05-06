81b776d36f8907e91d4ce85a0844e22cd1104ec5
Omonom47 <56996234+Omonom47@users.noreply.github.com>
2020-05-06 18:51:03 +0200

    Manually added roomgeneration code

---
+++ Assets/Scripts/RoomGeneration/OnTriggerMoveTo.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerMoveTo : MonoBehaviour
+{
+    string tag;
+    string newDoor;
+    public Vector2 roomPosition;
+    Vector2 temp;
+
+    void FixedUpdate()
+    {
+        tag = gameObject.tag;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GridPosition[] GridPositions = Object.FindObjectsOfType<GridPosition>();
+        GameObject[] rooms = new GameObject[GridPositions.Length];
+        for (int i = 0; i < rooms.Length; i++)
+        {
+            rooms[i] = GridPositions[i].gameObject;
+        }
+        if (collision.CompareTag("Player"))
+        {
+            switch (tag)
+            {
+                case "R":
+                    newDoor = "L";
+                    temp = new Vector2(1, 0);
+                    break;
+                case "L":
+                    newDoor = "R";
+                    temp = new Vector2(-1, 0);
+                    break;
+                case "T":
+                    newDoor = "B";
+                    temp = new Vector2(0, 1);
+                    break;
+                case "B":
+                    newDoor = "T";
+                    temp = new Vector2(0, -1);
+                    break;
+                default:
+                    throw new System.ArgumentException("The door didn't have a tag that is R, L, T or B", "Invalid Tag");
+            }
+            foreach (GameObject room in rooms)
+            {
+                if (room.GetComponent<GridPosition>().gridPos == roomPosition + temp)
+                {
+                    Debug.Log(newDoor);
+                    collision.gameObject.transform.position = room.transform.Find(newDoor).position;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RoomGeneration/Room.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Room {
+	public Vector2 gridPos;
+	public int type;
+	public bool doorTop, doorBot, doorLeft, doorRight;
+	public string tag = "";
+	public Room(Vector2 _gridPos, int _type){
+		gridPos = _gridPos;
+		type = _type;
+	}
+	public void makeTag()
+	{
+		if (doorTop)
+		{
+			tag += "T";
+		} 
+		if (doorBot)
+		{
+			tag += "B";
+		}
+		if (doorLeft)
+		{
+			tag += "L";
+		}
+		if (doorRight)
+		{
+			tag += "R";
+		}
+	}
+}
