45980be3cdcaa3c690ebfaffeda67276f1494790
Omonom47 <56996234+Omonom47@users.noreply.github.com>
2020-05-07 10:14:37 +0200

    Testing for shooting has begun
    minor changes to bullet prefab
    BulletScript added

---
+++ Assets/Scripts/General/BulletScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    [SerializeField] float lifeTime = 5f;
+    [SerializeField] int damage = 5;
+
+    CharacterScript hitCharacter;
+    Animator anim;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+        anim = GetComponent<Animator>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        anim.SetTrigger("Hit");
+        if (collision.gameObject.GetComponent<CharacterScript>() != null)
+        {
+
+            hitCharacter = collision.gameObject.GetComponent<CharacterScript>();
+
+
+            if (!hitCharacter.WasHit)
+            {
+                hitCharacter.TakeDamage(damage);
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/ShootingScript.cs
+++ Assets/Scripts/Player/ShootingScript.cs
@@ -6,40 +6,49 @@ public class ShootingScript : MonoBehaviour
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] GameObject crosshair;
 
-    [SerializeField] float shootingCost = 2;
-    [SerializeField] float crosshairDistance = 1;
+    [SerializeField] float shootingCost = 2f;
+    [SerializeField] float crosshairDistance = 1f;
+    [SerializeField] float bulletSpeed = 1f;
+
     Vector2 crosshairDir;
     PlayerMovement pM;
+    bool endOfAiming;
 
     // Start is called before the first frame update
     void Start()
     {
         pM = GetComponent<PlayerMovement>();
+        //crosshair.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         crosshairDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if (Input.GetButton("Shoot"))
+        endOfAiming = Input.GetButtonUp("Shoot");
+        Aim();
+        Shoot();
+        /*if (Input.GetButton("Shoot"))
         {
             pM.canMove = false;
             pM.animator.SetBool("Aiming", true);
+            crosshair.SetActive(true);
             Aim();
         }
         else
         {
             pM.canMove = true;
-
-        }
+            crosshair.SetActive(false);
+        }*/
     }
 
     void Aim()
     {
         if (crosshairDir != Vector2.zero)
         {
-            crosshair.transform.localPosition = crosshairDir * crosshairDistance;
-
+            crosshair.transform.localPosition = (crosshairDir * crosshairDistance).normalized;
+            pM.animator.SetFloat("AimX", crosshairDir.x);
+            pM.animator.SetFloat("AimY", crosshairDir.y);
         }
     }
 
@@ -47,5 +56,10 @@ public class ShootingScript : MonoBehaviour
     {
         Vector2 shootingDirection = crosshair.transform.localPosition;
         shootingDirection.Normalize();
+        if (endOfAiming)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = shootingDirection * bulletSpeed;
+        }
     }
 }
