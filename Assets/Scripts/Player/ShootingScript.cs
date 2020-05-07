aa8aa6916f30a30991ea44a71dbf6119cdbe90ed
Omonom47 <56996234+Omonom47@users.noreply.github.com>
2020-05-07 09:08:55 +0200

    Added shootingScript & crosshair grafic

---
+++ Assets/Scripts/Player/ShootingScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class ShootingScript : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] GameObject crosshair;
+
+    [SerializeField] float shootingCost = 2;
+    [SerializeField] float crossHairDIstance = 1;
+    Vector2 aimDir;
+    PlayerMovement pM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pM = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Shoot"))
+        {
+            pM.canMove = false;
+        }
+        else
+        {
+            pM.canMove = true;
+        }
+    }
+
+    void Aim()
+    {
+
+    }
+
+    void Shoot()
+    {
+
+    }
+}
