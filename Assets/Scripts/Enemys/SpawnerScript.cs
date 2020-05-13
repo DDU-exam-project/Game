5f878d0ec76bfd1bc697c1344d42771bfaddef64
Omonom47 <56996234+Omonom47@users.noreply.github.com>
2020-05-13 21:04:28 +0200

    Made SpawnerScript to spawn enemies
    Added spawners to every roomtemplate

---
+++ Assets/Scripts/Enemys/SpawnerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    [SerializeField] float spawnChance = .5f;
+    [SerializeField] GameObject enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Random.value <= spawnChance)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            enemy.GetComponent<EnemyAI>().target = PlayerScript.player.transform;
+        }
+    }
+
+    
+}
--- Assets/Scripts/Player/ShootingScript.cs
+++ Assets/Scripts/Player/ShootingScript.cs
@@ -5,7 +5,6 @@ public class ShootingScript : MonoBehaviour
 {
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] GameObject crosshair;
-
     [SerializeField] int shootingCost = 2;
     [SerializeField] float crosshairDistance = 1f;
     [SerializeField] float bulletSpeed = 1f;
@@ -55,8 +54,7 @@ public class ShootingScript : MonoBehaviour
         {
             crosshair.transform.localPosition = crosshairDir * crosshairDistance;
             pM.animator.SetFloat("AimX", crosshairDir.x);
-            pM.animator.SetFloat("AimY", crosshairDir.y);
-            
+            pM.animator.SetFloat("AimY", crosshairDir.y);          
         }
     }
 
